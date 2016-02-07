f3ba65d6fd0ee6c1f98e1a41086ef5626fc9bec9
atousley <annette.tousley@gmail.com>
2016-02-07 17:52:25 -0600

    final js

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -1,7 +1,6 @@
 var clickCounter = -1;
 var personNum = 0;
-//var i = 0;
-var person = 0;
+
 
 $(document).ready(function(){
     getData();
@@ -15,38 +14,23 @@ function getData(){
             console.log(data);
 
             $.each(data.people, function(i, person) {
-                $('#showcase').append('<div class="person"></div>').data('id', i);
-
-                var $el = $('#showcase').children().last();
-                $el.append('<h2>' + person.name + '</h2>');
-                $el.append('<p>' + person.favoriteMovie1 + '</p>');
-                $el.append('<p>' + person.favoriteMovie2 + '</p>');
-                $el.append('<p>' + person.favoriteSong + '</p>');
-                $el.addClass('hidden');
-
                 $('.currentPerson').append('<button class="selectedPerson"> ' + personNum + ' </button>');
                 $('.currentPerson').children().last().data('id', i);
                 personNum++;
-
             });
 
-            $('.container').on('click', '.selectedPerson', function() {
+            $('.currentPerson').on('click', '.selectedPerson', function() {
                 $('.selectedPerson').each(function(i) {
                     $(this).removeClass('highlight');
                 });
 
                 $(this).addClass('highlight');
+                var selectedButton = $(this);
 
+                clickCounter = selectedButton.data('id');
+                i = clickCounter;
 
-                $('.person').each(function(i) {
-                    $(this).removeClass('selected');
-                    $(this).addClass('hidden');
-                    if ($(this).data('id') == $('.selectedPerson').data('id')) {
-                        $(this).removeClass(hidden);
-                        $(this).addClass('selected');
-                    }
-                });
-
+                showPerson(data.people[i]);
             });
 
             $('.rightScroll').on('click', function() {
@@ -59,7 +43,16 @@ function getData(){
                     clickCounter = -1;
                     showPerson(data.people[i]);
                 }
-                return clickCounter;
+
+                $('.selectedPerson').removeClass('highlight');
+
+                $('.selectedPerson').each(function() {
+                    var currentElement = $(this);
+
+                    if (currentElement.data('id') === i){
+                            $(this).addClass('highlight');
+                        }
+                });
             });
 
             $('.leftScroll').on('click', function() {
@@ -72,24 +65,27 @@ function getData(){
                     clickCounter = 19;
                     showPerson(data.people[i]);
                 }
-                return clickCounter;
+
+                $('.selectedPerson').removeClass('highlight');
+
+                $('.selectedPerson').each(function() {
+                    var currentElement = $(this);
+                    if (currentElement.data('id') === i){
+                        $(this).addClass('highlight');
+                    }
+                });
             });
 
-            //function showPerson(person) {
-            //    $('#showcase').children().last().addClass('hidden');
-            //    $('#showcase').append('<div class="person"></div>');
-            //
-            //    var $el = $('.person');
-            //    $el.append('<h2>' + person.name + '</h2>');
-            //    $el.append('<p>' + person.favoriteMovie1 + '</p>');
-            //    $el.append('<p>' + person.favoriteMovie2 + '</p>');
-            //    $el.append('<p>' + person.favoriteSong + '</p>');
-            //
-            //
-            //    //console.log(currentPerson);
-            //
-            //    selectPerson();
-            //};
+            function showPerson(person) {
+                $('#showcase').children().last().addClass('hidden');
+                $('#showcase').append('<div class="person"></div>');
+
+                var $el = $('.person');
+                $el.append('<h2>' + person.name + '</h2>');
+                $el.append('<p>Favorite Movie: ' + person.favoriteMovie1 + '</p>');
+                $el.append('<p>Second Favorite Movie: ' + person.favoriteMovie2 + '</p>');
+                $el.append('<p>Favorite Song: ' + person.favoriteSong + '</p>');
+            }
         },
         error: function() {
             console.log('ERROR: Unable to contact the server.');
