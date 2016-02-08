fbc27323d698a30921d30459113a5515bd34f4c4
atousley <annette.tousley@gmail.com>
2016-02-08 12:19:17 -0600

    refactored to separate functions from success, created new function for highlighting buttons

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -37,60 +37,53 @@ function getData(){
                 clickCounter++;
                 i = clickCounter;
 
-                if(clickCounter <= 18) {
-                    showPerson(data.people[i]);
-                } else if(clickCounter == 19){
+                if(clickCounter == data.people.length -1){
                     clickCounter = -1;
-                    showPerson(data.people[i]);
                 }
-
-                $('.selectedPerson').removeClass('highlight');
-
-                $('.selectedPerson').each(function() {
-                    var currentElement = $(this);
-
-                    if (currentElement.data('id') === i){
-                            $(this).addClass('highlight');
-                        }
-                });
+                showPerson(data.people[i]);
+                highlightButton();
             });
 
             $('.leftScroll').on('click', function() {
                 clickCounter--;
                 i = clickCounter;
 
-                if(clickCounter >= 0) {
-                    showPerson(data.people[i]);
-                } else {
-                    clickCounter = 19;
-                    showPerson(data.people[i]);
+                if(clickCounter < 0) {
+                    clickCounter = data.people.length;
                 }
 
-                $('.selectedPerson').removeClass('highlight');
-
-                $('.selectedPerson').each(function() {
-                    var currentElement = $(this);
-                    if (currentElement.data('id') === i){
-                        $(this).addClass('highlight');
-                    }
-                });
+                showPerson(data.people[i]);
+                highlightButton();
             });
 
-            function showPerson(person) {
-                $('#').find('span').remove();
-                $('#showcase').children().last().addClass('hidden');
-                $('#showcase').append('<div class="person"></div>');
-
-                var $el = $('.person');
-                $el.append('<h2>' + person.name + '</h2>');
-                $el.append('<p>Favorite Movie: ' + person.favoriteMovie1 + '</p>');
-                $el.append('<p>Second Favorite Movie: ' + person.favoriteMovie2 + '</p>');
-                $el.append('<p>Favorite Song: ' + person.favoriteSong + '</p>');
-            }
         },
         error: function() {
             console.log('ERROR: Unable to contact the server.');
         }
 
+    });
+}
+
+function showPerson(person) {
+    $('#showcase').find('span').remove();
+    $('#showcase').children().last().addClass('hidden');
+    $('#showcase').append('<div class="person"></div>');
+
+    var $el = $('.person');
+    $el.append('<h2>' + person.name + '</h2>');
+    $el.append('<p>Favorite Movie: ' + person.favoriteMovie1 + '</p>');
+    $el.append('<p>Second Favorite Movie: ' + person.favoriteMovie2 + '</p>');
+    $el.append('<p>Favorite Song: ' + person.favoriteSong + '</p>');
+}
+
+function highlightButton() {
+    $('.selectedPerson').removeClass('highlight');
+
+    $('.selectedPerson').each(function() {
+        var currentElement = $(this);
+
+        if (currentElement.data('id') === i){
+            $(this).addClass('highlight');
+        }
     });
 }
